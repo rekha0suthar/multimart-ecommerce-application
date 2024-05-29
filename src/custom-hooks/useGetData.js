7a729f99b585e69c5371fb0cc62867b0bad20004
REKHA SUTHAR <71004640+rekha0suthar@users.noreply.github.com>
2024-05-29 14:21:48 +0530

    Update useGetData.js

--- src/custom-hooks/useGetData.js
+++ src/custom-hooks/useGetData.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import  {db} from '../firebase.config'
 import { collection, onSnapshot } from "firebase/firestore";
 
@@ -17,7 +17,7 @@ const useGetData = (collectionName) => {
             
         }
         getData()
-    }, [])
+    }, [collectionRef])
   return { data, loading }
 }
 
